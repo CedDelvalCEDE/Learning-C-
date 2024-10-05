45a880202371b9e639fdd19a4afab69c78221d3d
guste <gusterdehui@gmail.com>
2024-10-05 11:06:32 +0200

    Exo_1_collection terminé

--- Program.cs
+++ Program.cs
@@ -1,20 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Microsoft.VisualBasic;
 
-/*
+// while nombre premier inférieur au nombre entré par l'user
 
-Le marché
-Créez un jeu de gestion de panier de fruits au marché à l'aide d'un tableau de chaînes de caractères.
-L'utilisateur peut ajouter maximum 5 fruits, les retirer, les afficher et les rechercher.
-Gérez les doublons lors de l'ajout.
-Permettez à l'utilisateur de quitter le jeu via le menu.
-
-*/
-
-void Market() 
+void coll_exo_1()
 {
-    Console.WriteLine("Bonjour et bienvenue au marché !!!");
-    Console.WriteLine("Vous pouvez cherché un produit et le mettre dans votre panier. \n 1. Ajouter un produit \n 2. Retirer un produit \n 3. Afficher mon panier \n 4. Rechercher un produit");
+    List<int> values = [];
+    Console.WriteLine("Entrez un nombre : ");
+    var entry = Console.ReadLine();
+    bool fine = int.TryParse(entry, out int entry_int);
+    int start_nb = 2;
+    if (fine && (entry_int > 1 || entry_int < -1)) {
+        while (start_nb <= entry_int)
+        {
+            int count = 0;
+            for (int i = 2; i <= entry_int; i++)
+            {
+                if (start_nb % i == 0) 
+                {
+                    count++;
+                }
+            }
+            if (count == 1 && start_nb > 1) {
+                values.Add(start_nb);
+            }
+            start_nb++;
+        }
+        foreach (int i in values) 
+        {
+            Console.Write($"| {i} |");
+        }
+    }
+    else {
+        Console.WriteLine("l'entrée n'est pas un nombre ou un nombre non divisable (0,1,-1).");
+    }
 }
+
+coll_exo_1();
--- exo_fini.cs
+++ exo_fini.cs
@@ -179,3 +179,23 @@ if (bban.Length == 12 && bban is string) {
 }
 
 */
+
+/*
+
+Le marché
+Créez un jeu de gestion de panier de fruits au marché à l'aide d'un tableau de chaînes de caractères.
+L'utilisateur peut ajouter maximum 5 fruits, les retirer, les afficher et les rechercher.
+Gérez les doublons lors de l'ajout.
+Permettez à l'utilisateur de quitter le jeu via le menu. ---> PAS FINI
+
+*/
+
+/*
+
+void Market() 
+{
+    Console.WriteLine("Bonjour et bienvenue au marché !!!");
+    Console.WriteLine("Vous pouvez cherché un produit et le mettre dans votre panier. \n 1. Ajouter un produit \n 2. Retirer un produit \n 3. Afficher mon panier \n 4. Rechercher un produit");
+}
+
+*/
